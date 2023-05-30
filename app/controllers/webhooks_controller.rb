a15d1955e2229d5df592adc5a6de8e10de7274f5
Anes Hodza <anes.hodza@renuo.ch>
2023-05-30 16:29:02 +0200

    Remove unnecessary logic

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -4,12 +4,6 @@ class WebhooksController < ApplicationController
   protect_from_forgery except: %i[github_webhook_catcher semaphore_webhook_catcher]
 
   def github_webhook_catcher
-    p_body = request.body.read
-
-    unless verify_signature(p_body, request.env['HTTP_X_HUB_SIGNATURE_256'])
-      return render json: {status: 403}, status: :forbidden
-    end
-
     github_webhook_handler(params)
 
     render json: {status: :ok}
@@ -29,12 +23,6 @@ class WebhooksController < ApplicationController
     return unless numbers.length.positive? && Issue.exists?(id: numbers[1])
 
     PullRequest.auto_create_or_update(params.merge(issue_id: numbers[1]))
-  end
-
-  def verify_signature(payload_body, recieved_signature)
-    signature = "sha256=#{OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha256'),
-                                                  ENV.fetch('GITHUB_WEBHOOK_SECRET', nil), payload_body)}"
-    Rack::Utils.secure_compare(signature, recieved_signature)
   end
 
   def semaphore_webhook_handler(params)
