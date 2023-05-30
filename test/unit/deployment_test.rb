fa5383565c65fd7ce9a2a361ed207ab5759c2355
Anes Hodza <anes.hodza@renuo.ch>
2023-05-30 17:02:39 +0200

    Add tests for new method

--- test/unit/deployment_test.rb
+++ test/unit/deployment_test.rb
@@ -7,4 +7,24 @@ class DeploymentTest < ActiveSupport::TestCase
     FactoryBot.create(:deployment)
     assert_equal 1, PullRequest.where(id: Deployment.first.pull_request_id).count
   end
+
+  def test_auto_create_or_update_updates_existing_deployment
+    FactoryBot.create(:pull_request)
+    deploy = FactoryBot.create(:deployment)
+
+    Deployment.auto_create_or_update(deploy.deploy_branch, deploy.pull_request_id, "aneshodza.ch", !deploy.has_passed)
+
+    assert_equal deploy.reload.url, "aneshodza.ch"
+  end
+
+  def test_auto_create_or_update_new_branch
+    FactoryBot.create(:pull_request)
+    deploy = FactoryBot.create(:deployment)
+
+    assert_difference 'Deployment.count', 1 do
+      Deployment.auto_create_or_update("some branch that doesn't exist", deploy.pull_request_id, deploy.url, deploy.has_passed)
+    end
+
+    assert_equal 2, Deployment.count
+  end
 end
