2d8e8ac5a9f1302c8108e914693197732779959d
Anes Hodza <anes.hodza@renuo.ch>
2023-05-31 14:54:09 +0200

    Fix list view

--- lib/issue_details_hook_listener.rb
+++ lib/issue_details_hook_listener.rb
@@ -2,32 +2,26 @@
 
 class NewSectionHookListener < Redmine::Hook::ViewListener
   def view_issues_show_description_bottom(context={})
-    p "some extra context:"
     @prs = ActiveRecord::Base.connection.exec_query("SELECT * FROM pull_requests WHERE issue_id = (#{context[:issue].id})").to_a
     @deployments = @prs.map do |pr|
       ActiveRecord::Base.connection.exec_query("SELECT * FROM deployments WHERE pull_request_id = (#{pr['id']})").to_a
     end
-    @deployments
-    @deployments_strings = @deployments.each_with_index.map do |deployment_list, index|
-      p deployment_list[0]
-      p deployment_list[0]['url']
-      deployment_list.each_with_index.map do |deployment, index|
-        <<-ListObject
-          <li>
-            <a href='#{deployment['url']}' target='_blank'>#{deployment['deploy_branch']}</a>
-          </li>
-        ListObject
+    @deployments_strings = []
+    @deployments.each do |deployment_list|
+      formatted_deployment_list = []
+      deployment_list.each do |deployment|
+        formatted_deployment_list << "<li><a href='#{deployment['url']}' target='_blank'>#{deployment['deploy_branch']}</a></li>"
       end
+      @deployments_strings << formatted_deployment_list
     end
 
-    p @deployments_strings
-
     @pr_string = @prs.each_with_index.map do |pr, index|
+      formatted_deployments_list = @deployments_strings[index].join
       <<-ListObject
       <li>
         <a href='#{pr['url']}' target='_blank'>#{pr['title']}</a>
         <ul>
-          #{@deployments_strings[index]}
+          #{formatted_deployments_list}
         </ul>  
       </li>
       ListObject
