49e81f88b20a42779e7a19973eb089ca98a06901
Anes Hodza <anes.hodza@renuo.ch>
2023-05-31 14:29:20 +0200

    Begin with list

--- lib/issue_details_hook_listener.rb
+++ lib/issue_details_hook_listener.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class NewSectionHookListener < Redmine::Hook::ViewListener
-  def view_issues_show_description_bottom(_context={})
+  def view_issues_show_description_bottom(context={})
+    p "some extra context:"
+    @prs = ActiveRecord::Base.connection.exec_query("SELECT * FROM pull_requests WHERE issue_id = (#{context[:issue].id})").to_a
+    @pr_string = @prs.map do |pr|
+      "<li><a href='#{pr['url']}'>#{pr['title']}</a></li>"
+    end.join
     content = <<-HTML
       <hr/>
-      <p><strong>Content added by hook</strong></p>
-      <span id='here'></span>
-      <script>document.getElementById('here').innerHTML = 'This was added with js';</script>
+      <p><strong>Pull Requests</strong></p>
+      <ul>
+        #{@pr_string}
+      </ul>
     HTML
     content
   end
